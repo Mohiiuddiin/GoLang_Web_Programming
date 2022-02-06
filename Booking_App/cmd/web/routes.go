ff49ea96f3e172fbc831d8b76eb66830bf397a8f
md.mohiiuddiin <md.mohiiuddiin@gmail.com>
2022-02-06 16:28:39 +0600

    main

--- Booking_App/cmd/web/routes.go
+++ Booking_App/cmd/web/routes.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"github.com/Mohiiuddiin/GoLang_Web_Programming/tree/main/Booking_App/pkg/config"
-	handler "github.com/Mohiiuddiin/GoLang_Web_Programming/tree/main/Booking_App/pkg/handlers"
 	"net/http"
 
+	"github.com/Mohiiuddiin/GoLang_Web_Programming/tree/main/Booking_App/pkg/config"
+	handler "github.com/Mohiiuddiin/GoLang_Web_Programming/tree/main/Booking_App/pkg/handlers"
 	// "github.com/bmizerany/pat"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -16,8 +16,8 @@ func routes(app *config.AppConfig) http.Handler {
 	// mux := pat.New()
 	// mux.Get("/home",http.HandlerFunc(handler.Repo.Home))
 	// mux.Get("/about",http.HandlerFunc(handler.Repo.About))
-
 	//using go-chi
+	
 	mux := chi.NewRouter()
 
 	mux.Use(middleware.Recoverer)
@@ -26,6 +26,9 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/Home", handler.Repo.Home)
 	mux.Get("/About", handler.Repo.About)
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static",fileServer))
 
 	return mux
 }
